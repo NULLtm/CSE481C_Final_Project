907658472e7d751bdc78359b19606da36f9ef4de
Owen Boseley <nulltm01@gmail.com>
2026-06-03 20:26:37 -0700

    ddsds

--- src/aaso_final_project/aaso_final_project/align_test.py
+++ src/aaso_final_project/aaso_final_project/align_test.py
@@ -51,7 +51,8 @@ class AutoChess(hm.HelloNode):
         # 1. Retract the arm (Blocking call)
         # This will pause the service callback until the arm has reached the goal
         self.get_logger().info('Moving arm to 0.0 (fully inward)...')
-        self.move_to_pose({'wrist_extension': 0.0}, blocking=True)
+        wrist_future = self.move_to_pose({'wrist_extension': 0.0}, blocking=False)
+        rclpy.spin_until_future_complete(self, wrist_future)
         self.get_logger().info('Arm retracted. Proceeding to base alignment.')
 
         # Failsafe 1: Check Joint States (Populated automatically by HelloNode)
